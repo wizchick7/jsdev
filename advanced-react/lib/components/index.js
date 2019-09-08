db2757d54b51de3049466618ed67db56e64a5145
Cindy Moline <cimoli@microsoft.com>
2019-09-08 07:02:18 -0700

    display test data

--- advanced-react/lib/components/index.js
+++ advanced-react/lib/components/index.js
@@ -2,25 +2,7 @@ import React from 'react';
 // ReactDom is used to interface with the browser
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  state = {
-    answer: 42
-  };
-
-  asyncFunc = () => {
-    return Promise.resolve(37);
-  }
-  async componentDidMount() {
-    this.setState({
-      answer: await this.asyncFunc()
-    });
-  }
-  render() {
-    return (
-      <h2>Hello Class Components -- { this.state.answer }</h2>
-    );
-  }
-}
+import App from './App';
 
 export default App;
 
